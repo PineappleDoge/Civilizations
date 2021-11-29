dde91e03c05f729a420fee917943e2517b069951
PineappleDoge <PineappleDoge2020@gmail.com>
2021-11-29 14:57:41 -0500

    Delete civs 9-27-21 directory

--- civs 9-27-21/src/ServerStorage/SawbladeScriptDump/README.lua
+++
@@ -1,5 +0,0 @@
---[[
-	I can't use Roblox's script editor for the life of me so I'm just going to have rojo sync into here and sync my stuff manually
-	
-	Don't edit stuff here. If it gets edited, it might get overwritten by rojo.
-]]
--- civs 9-27-21/src/StarterPlayer/StarterPlayerScripts/Client/Archive/UserInputController/InputStates/Controller.lua
+++
@@ -1,7 +0,0 @@
-return [[
-	Thumbstick1, Thumbstick2,
-	ButtonR2, ButtonL2, ButtonR1, ButtonL1
-	ButtonA, ButtonB, ButtonX, ButtonY, ButtonR3,	
-	ButtonL3, ButtonStart, ButtonSelect, DPadLeft,
-	DPadRight, DPadUp, DPadDown
-]]
